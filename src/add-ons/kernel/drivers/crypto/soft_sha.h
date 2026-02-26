4afa942c00ef50c1fb973eccc97f1502c42ff554
Fabio Tomat <f.t.public@gmail.com>
2026-02-26 10:56:02 +0100

    align sha contexts for hybrid acceleration use

--- src/add-ons/kernel/drivers/crypto/soft_sha.h
+++ src/add-ons/kernel/drivers/crypto/soft_sha.h
@@ -6,12 +6,34 @@
 #define _SOFT_SHA_H_
 
 #include <SupportDefs.h>
-
+/*
 struct SoftSHA256Context {
     uint32 state[8];
     uint64 count;
     uint8  buffer[64];
+};*/
+struct SoftSHA1Context {
+    uint32 state[5] __attribute__((aligned(16)));
+    uint64 count;
+    uint8  buffer[64] __attribute__((aligned(16)));
 };
+
+// SHA-256 (e 224) Context
+struct SoftSHA256Context {
+    uint32 state[8] __attribute__((aligned(16)));
+    uint64 count;
+    uint8  buffer[64] __attribute__((aligned(16)));
+};
+typedef struct SoftSHA256Context SoftSHA224Context;
+
+// SHA-512 (e 384) Context
+// Qui usiamo uint64 e allineamento a 32 per AVX2
+struct SoftSHA512Context {
+    uint64 state[8] __attribute__((aligned(32)));
+    uint64 count[2];
+    uint8  buffer[128] __attribute__((aligned(32)));
+};
+typedef struct SoftSHA512Context SoftSHA384Context;
 
 void soft_sha256_init(SoftSHA256Context* ctx);
 void soft_sha256_update(SoftSHA256Context* ctx, const uint8* input, size_t len);
