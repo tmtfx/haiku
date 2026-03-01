1b20a7cd854d9c97c6fc02b42dd72cf99f4ae45b
Fabio Tomat <f.t.public@gmail.com>
2026-03-01 10:34:15 +0100

    adapt to new fpu save, align roundkeys

--- src/add-ons/kernel/drivers/crypto/AESNI.h
+++ src/add-ons/kernel/drivers/crypto/AESNI.h
@@ -8,13 +8,15 @@
 
 #include <SupportDefs.h>
 #include <crypto/BCryptoDefs.h>
+#include <crypto/BCryptoKernelInternal.h>
 #include "BCryptoCore.h"
 
 #include <immintrin.h> // Necessario per __m128i
 
 struct AESNIContext {
-    __m128i encRoundKeys[15]; // Massimo 14 round per AES-256 + 1
-    __m128i decRoundKeys[15];
+    alignas(16) __m128i encRoundKeys[15]; // Massimo 14 round per AES-256 + 1
+    alignas(16) __m128i decRoundKeys[15];
+    alignas(16) BCryptoFPUContext fpu_save;
     int     rounds;
     uint8   iv[16];
 } __attribute__((aligned(16))); // L'allineamento a 16 byte è vitale per SSE
