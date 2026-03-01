121513ff6b0e1c8fa9001815953e0f5b21b32a9e
Fabio Tomat <f.t.public@gmail.com>
2026-03-01 10:36:40 +0100

    no static for public funcs

--- src/add-ons/kernel/drivers/crypto/HybridSHADigest.h
+++ src/add-ons/kernel/drivers/crypto/HybridSHADigest.h
@@ -12,20 +12,20 @@
 status_t BInitHybridSHADigest();
 
 status_t hybrid_sha_process(BCryptoRequest* request);
-static status_t hybrid_SHA1_init_bridge(void** context, size_t* contextSize);
-static status_t hybrid_SHA1_update_bridge(void* context, const iovec* vecs, size_t count);
-static status_t hybrid_SHA1_final_bridge(void* context, uint8* outDigest);
+status_t hybrid_SHA1_init_bridge(void** context, size_t* contextSize);
+status_t hybrid_SHA1_update_bridge(void* context, const iovec* vecs, size_t count);
+status_t hybrid_SHA1_final_bridge(void* context, uint8* outDigest);
 
-static status_t hybrid_SHA224_init_bridge(void** context, size_t* contextSize);
-static status_t hybrid_SHA256_init_bridge(void** context, size_t* contextSize);
-static status_t hybrid_SHA256_update_bridge(void* context, const iovec* vecs, size_t count);
-static status_t hybrid_SHA256_final_bridge(void* context, uint8* outDigest);
-static status_t hybrid_SHA224_final_bridge(void* context, uint8* outDigest);
+status_t hybrid_SHA224_init_bridge(void** context, size_t* contextSize);
+status_t hybrid_SHA256_init_bridge(void** context, size_t* contextSize);
+status_t hybrid_SHA256_update_bridge(void* context, const iovec* vecs, size_t count);
+status_t hybrid_SHA256_final_bridge(void* context, uint8* outDigest);
+status_t hybrid_SHA224_final_bridge(void* context, uint8* outDigest);
 
-static status_t hybrid_SHA384_init_bridge(void** context, size_t* contextSize);
-static status_t hybrid_SHA512_init_bridge(void** context, size_t* contextSize);
-static status_t hybrid_SHA512_update_bridge(void* context, const iovec* vecs, size_t count);
-static status_t hybrid_SHA512_final_bridge(void* context, uint8* outDigest);
-static status_t hybrid_SHA384_final_bridge(void* context, uint8* outDigest);
+status_t hybrid_SHA384_init_bridge(void** context, size_t* contextSize);
+status_t hybrid_SHA512_init_bridge(void** context, size_t* contextSize);
+status_t hybrid_SHA512_update_bridge(void* context, const iovec* vecs, size_t count);
+status_t hybrid_SHA512_final_bridge(void* context, uint8* outDigest);
+status_t hybrid_SHA384_final_bridge(void* context, uint8* outDigest);
 #endif // HYBRID_BLAKE2_DIGEST_H
 
