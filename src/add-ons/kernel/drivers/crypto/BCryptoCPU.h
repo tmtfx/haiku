9212ba751466e0fc07a98032ca2a81cd4d0118a3
Fabio Tomat <f.t.public@gmail.com>
2026-03-03 06:33:39 +0100

    don't define as compiler can skip something

--- src/add-ons/kernel/drivers/crypto/BCryptoCPU.h
+++ src/add-ons/kernel/drivers/crypto/BCryptoCPU.h
@@ -8,6 +8,7 @@
 #include <crypto/BCryptoDefs.h>
 #include <crypto/BCryptoKernelInternal.h>
 #include <x86gprintrin.h>
+//#include <KernelExport.h>
 
 extern bool gHasXsave;
 extern uint64 gXsaveMask;
@@ -16,23 +17,6 @@ typedef struct alignas(64) {
     uint8_t buffer[1024]; // 512 minimi, 1024 per sicurezza con AVX/XSAVE
 } fpu_state_t;
 
-#define B_PREPARE_CPU_STATE() \
-    cpu_status cpu_state = disable_interrupts(); \
-    bcrypto_save_regs(&ctx->fpu_save);
-    
-#define B_RESTORE_CPU_STATE() \
-    bcrypto_restore_regs(&ctx->fpu_save); \
-    restore_interrupts(cpu_state);
-/*
-#define B_PREPARE_CPU_STATE() \
-    fpu_state_t fpu_save; \
-    cpu_status cpu_state = disable_interrupts(); \
-    _fxsave(&fpu_save);
-
-#define B_RESTORE_CPU_STATE() \
-    _fxrstor(&fpu_save); \
-    restore_interrupts(cpu_state);
-*/
 /*
  * Rileva le capacità crittografiche della CPU (AES-NI, SHA, VIA PadLock)
  * e riempie la struttura crypto_device_info fornita.
