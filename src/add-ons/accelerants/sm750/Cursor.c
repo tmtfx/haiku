a807d751546ec28f99e5585c939c6bc4d1ba7223
Fabio Tomat <f.t.public@gmail.com>
2026-04-12 17:57:37 +0200

    handle panel/crt hw cursor

--- src/add-ons/accelerants/sm750/Cursor.c
+++ src/add-ons/accelerants/sm750/Cursor.c
@@ -15,27 +15,34 @@ sm750_move_cursor(uint16 x, uint16 y)
     shared_info *si = gInfo->si;
     vuint32 *regs = gInfo->regs;
 
-    uint16 x_pos = x - si->cursor.hot_x;
-    uint16 y_pos = y - si->cursor.hot_y;
+    uint32 x_pos = (x - si->cursor.hot_x) & 0x07FF;
+    uint32 y_pos = (y - si->cursor.hot_y) & 0x07FF;
     
     si->cursor.x = x;
     si->cursor.y = y;
 
-    DISP_W(CRT_CUR_POS, (x_pos << 16) | y_pos);
+    if (si->card_info.is_panel)
+        SM750_WREG32(SM750_DISP_PANEL_CUR_POS, (x_pos << 16) | y_pos);
+    else
+        SM750_WREG32(SM750_DISP_CRT_CUR_POS, (x_pos << 16) | y_pos);
 }
 
 void
 sm750_show_cursor(bool is_visible)
 {
+    shared_info *si = gInfo->si;
     vuint32 *regs = gInfo->regs;
-    uint32 ctrl = DISP_R(CRT_CUR_CTRL);
+    
+    uint32 reg = si->card_info.is_panel ? SM750_DISP_PANEL_CUR_ADDR : SM750_DISP_CRT_CUR_ADDR;
+    
+    uint32 ctrl = SM750_REG32(reg);
     
     if (is_visible)
-        ctrl |= (1 << 31); 
+        ctrl |= (1 << 31); // Enable bit
     else
         ctrl &= ~(1 << 31);
         
-    DISP_W(CRT_CUR_CTRL, ctrl);
+    SM750_WREG32(reg, ctrl);
 }
 
 status_t
@@ -44,15 +51,14 @@ sm750_set_cursor_shape(uint16 width, uint16 height, uint16 hot_x, uint16 hot_y,
 {
     shared_info *si = gInfo->si;
     vuint32 *regs = gInfo->regs;
-
-    // 1. Destinazione: fine della VRAM
-    uint32 cursorOffset = si->card_info.mem_size - 1024;
+    
+    // 1. Destinazione in VRAM (ultimi 2KB)
+    uint32 cursorOffset = si->card_info.mem_size - 2048;
     uint8 *dest = (uint8 *)si->framebuffer + cursorOffset;
 
-    // 2. Pulizia (Trasparente)
     memset(dest, 0, 1024);
 
-    // 3. Bit-packing 2bpp per SM750
+    // 2. Bit-packing (rimane invariato, è il formato hardware SMI)
     for (int y = 0; y < height && y < 64; y++) {
         uint8 *line_dest = dest + (y * 16); 
         for (int x = 0; x < width && x < 64; x++) {
@@ -60,27 +66,27 @@ sm750_set_cursor_shape(uint16 width, uint16 height, uint16 hot_x, uint16 hot_y,
             int bit_idx = 7 - (x % 8);
             
             bool a = (and_mask[byte_idx] >> bit_idx) & 1;
-            bool x = (xor_mask[byte_idx] >> bit_idx) & 1;
+            bool x_m = (xor_mask[byte_idx] >> bit_idx) & 1;
 
-            // Formato standard SMI: and=0, xor=0 -> Color0; and=1, xor=1 -> Invert...
-            // Noi semplifichiamo per Nero (10) e Bianco (11), resto trasparente (00)
-            if (!a) { // Pixel opaco
-                uint8 val = x ? 0x03 : 0x02; // 11 (Bianco) o 10 (Nero)
+            if (!a) { 
+                uint8 val = x_m ? 0x03 : 0x02; 
                 line_dest[x / 4] |= (val << (2 * (3 - (x % 4))));
             }
         }
     }
 
-    // 4. Update Registri tramite Macro
-    DISP_W(CRT_CUR_ADDR, cursorOffset | (1 << 31)); // Bit 31 = External VRAM
-    DISP_W(CRT_CUR_COLOR12, (0xFFFF << 16) | 0x0000); // Bianco (16 bit) | Nero (16 bit)
-    DISP_W(CRT_CUR_COLOR3, 0x0000FFFF);
+    // 3. Update Registri in base all'uscita attiva
+    if (si->card_info.is_panel) {
+        SM750_WREG32(SM750_DISP_PANEL_CUR_ADDR, (cursorOffset & 0x03FFFFFF) | (1 << 31));
+        SM750_WREG32(SM750_DISP_PANEL_CUR_COLOR12, (0xFFFF << 16) | 0x0000);
+        SM750_WREG32(SM750_DISP_PANEL_CUR_COLOR3, 0x0000FFFF);
+    } else {
+        SM750_WREG32(SM750_DISP_CRT_CUR_ADDR, (cursorOffset & 0x03FFFFFF) | (1 << 31));
+        SM750_WREG32(SM750_DISP_CRT_CUR_COLOR12, (0xFFFF << 16) | 0x0000);
+        SM750_WREG32(SM750_DISP_CRT_CUR_COLOR3, 0x0000FFFF);
+    }
 
-    // Salviamo hotspot per move_cursor
     si->cursor.hot_x = hot_x;
     si->cursor.hot_y = hot_y;
-    si->cursor.width = width;
-    si->cursor.height = height;
-
     return B_OK;
 }
