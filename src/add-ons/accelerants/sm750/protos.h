38219703e2e8ed19cbdb32dddb96d60aa37fcf35
Fabio Tomat <f.t.public@gmail.com>
2026-04-14 14:25:51 +0200

    update protos.h

--- src/add-ons/accelerants/sm750/protos.h
+++ src/add-ons/accelerants/sm750/protos.h
@@ -16,6 +16,8 @@ status_t sm750_propose_display_mode(display_mode *target,
                                     const display_mode *low, 
                                     const display_mode *high);
 uint32 sm750_accelerant_mode_count(void);
+status_t sm750_get_preferred_mode(display_mode* mode);
+status_t create_mode_list_from_edid(uint8* raw_buffer);
 status_t sm750_get_mode_list(display_mode* dm);
 status_t sm750_get_frame_buffer_config(frame_buffer_config *config);
 status_t sm750_set_cursor_shape(uint16 width, uint16 height, uint16 hot_x, uint16 hot_y, const uint8 *and_mask, const uint8 *xor_mask);
