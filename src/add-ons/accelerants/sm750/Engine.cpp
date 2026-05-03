a2b7e853f6c537094ee3910bd605440017d87e1f
Fabio Tomat <f.t.public@gmail.com>
2026-05-03 13:42:42 +0200

    TEST: keep both plls up

--- src/add-ons/accelerants/sm750/Engine.cpp
+++ src/add-ons/accelerants/sm750/Engine.cpp
@@ -111,8 +111,11 @@ void sm750_program_pll(uint32 target_khz, bool is_panel)
     // impostiamo entrambi i pll alla stessa frequenza giusto per vedere se cambia qualcosa
     SM750_WREG32(SM750_DISP_PANEL_PLL,pll_reg);
     SM750_WREG32(SM750_DISP_CRT_PLL,pll_reg);
-    uint32 ctrl_offset = is_panel ? SM750_CRT_CONTROL : SM750_PANEL_CONTROL;
-    SM750_WREG32(ctrl_offset,0x0); // disattiva tutto in ctr o in panel
+    //decommentare  se non va bene
+    //uint32 ctrl_offset = is_panel ? SM750_CRT_CONTROL : SM750_PANEL_CONTROL;
+    // tentativo 1 lasciare attivi entrambi i pll
+    //pll_reg &= ~(1 << 17); // PLL Power down
+    //SM750_WREG32(ctrl_offset,pll_reg); // disattiva il pll in ctr o in panel
     snooze(1500); 
     debug_printf("SM750_PLL: Programmazione %s a %u kHz\n", is_panel ? "PANEL" : "CRT", target_khz);
     debug_printf("SM750_PLL: M:%u N:%u Div:%u -> Reg 0x%08x\n", m, n, div_val, pll_reg);
@@ -137,7 +140,6 @@ status_t sm750_move_display_area(uint16 h_display_start, uint16 v_display_start)
 
 /* Limiti del Pixel Clock (Richiesto!) */
 status_t sm750_get_pixel_clock_limits(display_mode *dm, uint32 *low, uint32 *high) {
-	CALLED();
     *low = 10000;   // 10 MHz
     *high = 300000; // 300 MHz
     return B_OK;
