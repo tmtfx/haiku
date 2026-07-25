f76dda6030668c2f893a3c025bacd07c0f7372bd
Fabio Tomat <f.t.public@gmail.com>
2026-07-25 11:25:14 +0000

    disable overlay, no hw specs available

--- src/add-ons/accelerants/trident/hooks.cpp
+++ src/add-ons/accelerants/trident/hooks.cpp
@@ -58,15 +58,15 @@ get_accelerant_hook(uint32 feature, void* data)
 		case B_SYNC_TO_TOKEN:			return (void*)SyncToToken;
 
 		// Overlay
-		case B_OVERLAY_COUNT:				return (void*)trident_overlay_count;
-		case B_OVERLAY_SUPPORTED_SPACES:	return (void*)trident_overlay_supported_spaces;
-		case B_OVERLAY_SUPPORTED_FEATURES:	return (void*)trident_overlay_supported_features;
-		case B_ALLOCATE_OVERLAY_BUFFER:		return (void*)trident_allocate_overlay_buffer;
-		case B_RELEASE_OVERLAY_BUFFER:		return (void*)trident_release_overlay_buffer;
-		case B_GET_OVERLAY_CONSTRAINTS:		return (void*)trident_get_overlay_constraints;
-		case B_ALLOCATE_OVERLAY:			return (void*)trident_allocate_overlay;
-		case B_RELEASE_OVERLAY:				return (void*)trident_release_overlay;
-		case B_CONFIGURE_OVERLAY:			return (void*)trident_configure_overlay;
+		case B_OVERLAY_COUNT:				return NULL; //(void*)trident_overlay_count;
+		case B_OVERLAY_SUPPORTED_SPACES:	return NULL; //(void*)trident_overlay_supported_spaces;
+		case B_OVERLAY_SUPPORTED_FEATURES:	return NULL; //(void*)trident_overlay_supported_features;
+		case B_ALLOCATE_OVERLAY_BUFFER:		return NULL; //(void*)trident_allocate_overlay_buffer;
+		case B_RELEASE_OVERLAY_BUFFER:		return NULL; //(void*)trident_release_overlay_buffer;
+		case B_GET_OVERLAY_CONSTRAINTS:		return NULL; //(void*)trident_get_overlay_constraints;
+		case B_ALLOCATE_OVERLAY:			return NULL; //(void*)trident_allocate_overlay;
+		case B_RELEASE_OVERLAY:				return NULL; //(void*)trident_release_overlay;
+		case B_CONFIGURE_OVERLAY:			return NULL; //(void*)trident_configure_overlay;
 
 		// DPMS
 		case B_DPMS_CAPABILITIES:			return (void*)trident_dpms_capabilities;
