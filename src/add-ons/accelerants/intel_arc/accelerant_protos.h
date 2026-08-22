31a844b528cdbc8d1d79c10b49b3447715431a36
Fabio Tomat <f.t.public@gmail.com>
2026-08-22 13:17:03 +0200

    add overlay funcs

--- src/add-ons/accelerants/intel_arc/accelerant_protos.h
+++ src/add-ons/accelerants/intel_arc/accelerant_protos.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <Accelerant.h>
+#include <video_overlay.h>
 
 
 #ifdef __cplusplus
@@ -45,6 +46,19 @@ status_t	intel_arc_get_edid_info(void* info, size_t size, uint32* version);
 status_t	intel_arc_get_frame_buffer_config(frame_buffer_config* config);
 status_t	intel_arc_get_pixel_clock_limits(display_mode* mode, uint32* low,
 				uint32* high);
+uint32		intel_arc_overlay_count(const display_mode* mode);
+const uint32*	intel_arc_overlay_supported_spaces(const display_mode* mode);
+uint32		intel_arc_overlay_supported_features(uint32 colorSpace);
+const overlay_buffer* intel_arc_allocate_overlay_buffer(color_space colorSpace,
+				uint16 width, uint16 height);
+status_t	intel_arc_release_overlay_buffer(const overlay_buffer* buffer);
+status_t	intel_arc_get_overlay_constraints(const display_mode* mode,
+				const overlay_buffer* buffer, overlay_constraints* constraints);
+overlay_token	intel_arc_allocate_overlay(void);
+status_t	intel_arc_release_overlay(overlay_token token);
+status_t	intel_arc_configure_overlay(overlay_token token,
+				const overlay_buffer* buffer, const overlay_window* window,
+				const overlay_view* view);
 
 #ifdef __cplusplus
 }
